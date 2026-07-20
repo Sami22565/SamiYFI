584efdc786d4104f258d8b6cfcb044d3b9765012
Sami22565 <Samiyafeai80@gmail.com>
2026-07-20 16:56:13 -0500

    Add files via upload

---
+++ components/content/Card.d.ts
@@ -0,0 +1,10 @@
+export interface CardProps {
+  children: React.ReactNode;
+  hoverable?: boolean;
+  style?: React.CSSProperties;
+}
+
+/**
+ * @startingPoint section="Components" subtitle="Generic dark surface container" viewport="700x220"
+ */
+export function Card(props: CardProps): JSX.Element;
